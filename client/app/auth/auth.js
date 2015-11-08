52ce266265cec696c0fc887beb4692ee17b06683
DhiMalo <aherisan@gmail.com>
2015-11-07 18:56:15 -0800

    Added user token conditions and inline comments.

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,5 +1,6 @@
-// do not tamper with this code in here, study it, but do not touch
-// this Auth controller is responsible for our client side authentication
+// Added updates.
+
+//This Auth controller is responsible for our client side authentication
 // in our signup/signin forms using the injected Auth service
 angular.module('shortly.auth', [])
 
@@ -9,11 +10,11 @@ angular.module('shortly.auth', [])
   $scope.signin = function () {
     Auth.signin($scope.user)
       .then(function (token) {
-        if (token){ 
-          console.log(token);
+        if (token){   // Added a condition: If a token exists,
+          console.log('You are logged in.');  // ...we log to the console that the user is logged in.
           $window.localStorage.setItem('com.shortly', token);
           $location.path('/links');
-        } else {
+        } else {  // If not, we have gated the condition so that without token, the user cannot visit the links shortener page or see their myShortener page.  They are instead redirected to "signup".
           console.log('This user account has not yet been established. ')
           $location.path('/signup');
         }
