17fe9f952e25b11201860ab8ee5fc7272e86e1f3
DhiMalo <aherisan@gmail.com>
2015-11-07 18:13:51 -0800

    Provided inline explanatory code comments.

--- server/links/linkModel.js
+++ server/links/linkModel.js
@@ -1,24 +1,23 @@
 var mongoose = require('mongoose'),
-    crypto   = require('crypto');
+    crypto   = require('crypto'); 
 
 var LinkSchema = new mongoose.Schema({
  visits: Number,
- link: String, //...shortly.api/links/02853 = 
+ link: String, // ...shortly.api/links/02853...
  title: String,
- code: String,   //02853
+ code: String,   // eg. 02853...
  base_url: String,
- url: String //actual address of the information
+ url: String // actual address of the information
 });
 
 var createSha = function(url) {
-  // creating a SHA to store in place of password
-  var shasum = crypto.createHash('sha1');
+  var shasum = crypto.createHash('sha1');   // Invokes a secure hash algorithn (SHA) to generate a code that will replace the long URL. The heart of short.ly
   shasum.update(url);
   return shasum.digest('hex').slice(0, 5);
 };
 
 LinkSchema.pre('save', function (next){
-  var code = createSha(this.url);
+  var code = createSha(this.url); //  code is stored instead before saving url to the database (hence the term 'pre-save').
   this.code = code;
   next();
 });
