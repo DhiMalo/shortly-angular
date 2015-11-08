d5e81d12ff6a2b78ba6732e5f2ce180a2dc87e0b
DhiMalo <aherisan@gmail.com>
2015-11-07 18:43:16 -0800

    Added inline explanations and reindented code lines.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,40 +2,19 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, $http, Links) {
 
-  $scope.data = {
-    
-  };
+  // Instantiating the "Links" factory's "getLinks" function.  We can use it to assign a new property on the data.
+  // First we create the $scope.data variable.  Now, a property on that object can holds the result of this function's execution.
+
+  $scope.data = {}; 
 
   $scope.getLinks = function(){
-                      Links.getLinks()
-                      .then(function (data) {                      
-                      $scope.data.links = data;
-                      console.log('data.links:',$scope.data.links);
-      });
-                    }
+        Links.getLinks()
+        .then(function (data) {  // Lines 13-15 will allow asynchronous data to be populated via a 'promise' (hence the ".then" method.                   
+          $scope.data.links = data;
+          console.log('data.links:',$scope.data.links);
+    });
+  }
 
   $scope.getLinks();
 
 }); 
-
-
-//$scope.data.links = UNDEFINED:
-
-//   $scope.data = {
-    
-//   };
-
-//   $scope.getLinks = function(){
-//                       Links.getLinks()
-//                       .then(function (resp) {
-//                       console.log('insidegetlinks');
-//                       console.log('respdata', resp.data);
-//                       return resp.data;
-//       });
-//                     }
-
-//   // $scope.link = {};           
-  
-//   $scope.data.links = $scope.getLinks();
-
-// });
