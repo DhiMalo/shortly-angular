e08afeefce392dfdf1a6a465c1068405a9d2749e
DhiMalo <aherisan@gmail.com>
2015-11-07 18:17:36 -0800

    Code cleanup.

--- server/links/linkController.js
+++ server/links/linkController.js
@@ -1,29 +1,30 @@
 var Link    = require('./linkModel.js'),
-    Q       = require('q'),
-    util    = require('../config/utils.js');
+Q       = require('q'),
+util    = require('../config/utils.js');
 
 
 module.exports = {
+  
   findUrl: function (req, res, next, code) {
     var findLink = Q.nbind(Link.findOne, Link);
     findLink({code: code})
-      .then(function (link) {
-        if (link) {
-          req.navLink = link;
-          next();
-        } else {
-          next(new Error('Link not added yet'));
-        }
-      })
-      .fail(function (error) {
-        next(error);
-      });
+    .then(function (link) {
+      if (link) {
+        req.navLink = link;
+        next();
+      } else {
+        next(new Error('Link not added yet'));
+      }
+    })
+    .fail(function (error) {
+      next(error);
+    });
   },
 
   allLinks: function (req, res, next) {
-  var findAll = Q.nbind(Link.find, Link);
+    var findAll = Q.nbind(Link.find, Link);
 
-  findAll({})
+    findAll({})
     .then(function (links) {
       res.json(links);
     })
@@ -34,7 +35,6 @@ module.exports = {
 
   newLink: function (req, res, next) { 
     var url = req.body.url;
-    console.log(req.body);
     if (!util.isValidUrl(url)) {
       return next(new Error('Not a valid url'));
     }
@@ -43,32 +43,32 @@ module.exports = {
     var findLink = Q.nbind(Link.findOne, Link);
 
     findLink({url: url})
-      .then(function (match) {
-        if (match) {
-          res.send(match);
-        } else {
-          return  util.getUrlTitle(url);
-        }
-      })
-      .then(function (title) {
-        if (title) {
-          var newLink = {
-            url: url,
-            visits: 0,
-            base_url: req.headers.origin,
-            title: title
-          };
-          return createLink(newLink);
-        }
-      })
-      .then(function (createdLink) {
-        if (createdLink) {
-          res.json(createdLink);
-        }
-      })
-      .fail(function (error) {
-        next(error);
-      });
+    .then(function (match) {
+      if (match) {
+        res.send(match);
+      } else {
+        return  util.getUrlTitle(url);
+      }
+    })
+    .then(function (title) {
+      if (title) {
+        var newLink = {
+          url: url,
+          visits: 0,
+          base_url: req.headers.origin,
+          title: title
+        };
+        return createLink(newLink);
+      }
+    })
+    .then(function (createdLink) {
+      if (createdLink) {
+        res.json(createdLink);
+      }
+    })
+    .fail(function (error) {
+      next(error);
+    });
   },
 
   navToLink: function (req, res, next) {
