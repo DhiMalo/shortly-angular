4d5bbaa7a86f0aec44d32dcea101b889598dfb1b
DhiMalo <aherisan@gmail.com>
2015-11-07 18:30:48 -0800

    Cleaned up code and added implementation comments.

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,19 +1,15 @@
-angular.module('shortly.shorten', [])
+angular.module('shortly.shorten', []) 
 
 .controller('ShortenController', function ($scope, $location, Links) {
   
-  $scope.link = {};
-
+  $scope.link = {}; // We establish the $scope.link variable, assign an object and pass that object our methods from the "factory" (in services.js).
 
   $scope.addLink = function(){
                         console.log('scopelink:', $scope.link);
                         Links.addLink($scope.link)
                         .then(function (data) { 
                           console.log('scopeaddlink', data);
-                        // $scope.link;
-        });
-                      }
-
-  // $scope.addLink();
+                        });
+  }
 
 });
